76fc5ea80a9c8ae4d508cb5c67fef97752fa97cf
jpfox156 <jpfox156@yahoo.com.au>
2026-01-14 23:20:49 +1000

    Update composejob.go
    more typos

--- core/composejob.go
+++ core/composejob.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/gobs/args"
@@ -56,8 +57,8 @@ func (j *ComposeJob) buildCommand(ctx *Context) (*exec.Cmd, error) {
 
 	//Sanitize other fields
 	sanitizer := config.NewSanitizer()
-	j.Project = sanitizer.SanitizeString(j.Project, 256)
-	j.Profile = sanitizer.SanitizeString(j.Profile, 256)
+	j.Project, err := sanitizer.SanitizeString(j.Project, 256)
+	j.Profile, err := sanitizer.SanitizeString(j.Profile, 256)
 	
 	// Build docker compose command
 	var cmdArgs []string
