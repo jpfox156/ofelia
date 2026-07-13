abbd9e46c3fd7bb65ef7489eee160d1b32cc65ce
jpfox156 <jpfox156@yahoo.com.au>
2026-07-13 23:21:45 +1000

    Update composejob.go

--- core/composejob.go
+++ core/composejob.go
@@ -99,7 +99,7 @@ func (j *ComposeJob) buildCommand(ctx *Context) (*exec.Cmd, error) {
 		return nil, fmt.Errorf("look path %q: %w", cmdArgs[0], err)
 	}
 
-	config.logger.Debug("prepared composejob", bin, fmt.Printf(j.Environment, "%v") + cmdArgs) 
+	config.logger.Debug("prepared composejob", bin, fmt.Printf("%v", j.Environment) + cmdArgs) 
 
 	return &exec.Cmd{
 		Path:   bin,
